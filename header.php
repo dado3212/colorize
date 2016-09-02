0e07f651b5cf1c0436cee21bbc80dc54c326486d
Alex Beals <alexcbeals@gmail.com>
2016-09-02 06:01:06 +0000

    added google analytics

--- header.php
+++ header.php
@@ -36,6 +36,18 @@
 		<meta property="og:type" content="website">
 		<meta property="og:site_name" content="AlexBeals.com">
 
+		<!-- Google Analytics -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-70745807-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+
 		<link href="https://fonts.googleapis.com/css?family=Oswald:300,400|Open+Sans" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="css/nav.css">
 		<?php
